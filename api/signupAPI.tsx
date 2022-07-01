fe9dbeb052a97d275a521b5859c87827472b5161
Eli Tracy <elitracy19@gmail.com>
2022-06-30 19:38:02 -0700

    refactor(signup.tsx): moved signupAPI to /api

---
+++ api/signupAPI.tsx
@@ -0,0 +1,26 @@
+// signupAPI - Params(username:string,password:string,email:string,name:string,setUsernameExists:function,setEmailExists:function)
+export default async function signupAPI(
+  username: string,
+  password: string,
+  email: string,
+  name: string,
+  setUsernameExists: any,
+  setEmailExists: any
+) {
+  return fetch(
+    `https://profesy.herokuapp.com/signup?username=${username}&password=${password}&email=${email}&name=${name}`
+  )
+    .then((res) => {
+      return res.json()
+    })
+    .then((data) => {
+      if (data.userInsert === 0) {
+        setEmailExists(data.emailExists)
+        setUsernameExists(data.usernameExists)
+      }
+      return data
+    })
+    .catch((err) => {
+      console.error(err)
+    })
+}
--- components/screens/Signup.tsx
+++ components/screens/Signup.tsx
@@ -16,35 +16,9 @@ import { colors } from '../../utils/colors'
 import { sha256 } from 'js-sha256'
 import React from 'react'
 import { getItem, storeItem } from '../../utils/localStorage'
+import signupAPI from '../../api/signupAPI'
 
 type signupScreenProp = NativeStackNavigationProp<RootStackParamList, 'Signup'>
-
-// signupAPI - Params(username:string,password:string,email:string,name:string,setUsernameExists:function,setEmailExists:function)
-async function signupAPI(
-  username: string,
-  password: string,
-  email: string,
-  name: string,
-  setUsernameExists: any,
-  setEmailExists: any
-) {
-  return fetch(
-    `https://profesy.herokuapp.com/signup?username=${username}&password=${password}&email=${email}&name=${name}`
-  )
-    .then((res) => {
-      return res.json()
-    })
-    .then((data) => {
-      if (data.userInsert === 0) {
-        setEmailExists(data.emailExists)
-        setUsernameExists(data.usernameExists)
-      }
-      return data
-    })
-    .catch((err) => {
-      console.error(err)
-    })
-}
 
 export function Signup() {
   // SET STATES
